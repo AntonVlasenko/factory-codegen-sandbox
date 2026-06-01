accd9d92483d6e3a1459416f7d9f88876cadbf1a
Factory v2 <factory-v2@example.invalid>
2026-06-01 08:43:22 +0000

    factory-v2 task 361b9220-c12a-425e-bd14-d01d00d90b87

--- test_app.py
+++ test_app.py
@@ -1,6 +1,11 @@
-from app import add
+from app import add, eleventuple
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_eleventuple():
+    assert eleventuple(2) == 22
+    assert eleventuple(-1) == -11
