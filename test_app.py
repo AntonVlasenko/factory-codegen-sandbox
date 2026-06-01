05489ff673697dd16400be3c0fc88b9bfe8fc0ea
Factory v2 <factory-v2@example.invalid>
2026-06-01 07:34:28 +0000

    factory-v2 task e4dd8e19-dc45-41a3-8bf6-ef5fc266789d

--- test_app.py
+++ test_app.py
@@ -1,6 +1,12 @@
-from app import add
+from app import add, triple
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_triple():
+    assert triple(2) == 6
+    assert triple(-4) == -12
+    assert triple(0) == 0
