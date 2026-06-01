a1954406eb2a4c9011bc8aa4ad852b3c3f304da3
Factory v2 <factory-v2@example.invalid>
2026-06-01 08:37:51 +0000

    factory-v2 task 694479c5-57c5-4a83-8ae5-03ef60f3361b

--- test_app.py
+++ test_app.py
@@ -1,6 +1,12 @@
-from app import add
+from app import add, octuple
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_octuple():
+    assert octuple(3) == 24
+    assert octuple(-2) == -16
+    assert octuple(0) == 0
