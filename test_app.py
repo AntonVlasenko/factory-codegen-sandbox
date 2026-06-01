56b14dd05fcf2f751675283543a1241e61719f9f
Factory v2 <factory-v2@example.invalid>
2026-06-01 07:35:04 +0000

    factory-v2 task 25de33b7-de27-4dce-8393-61de2b3bd1bb

--- test_app.py
+++ test_app.py
@@ -1,6 +1,12 @@
-from app import add
+from app import add, square
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_square():
+    assert square(3) == 9
+    assert square(-4) == 16
+    assert square(0) == 0
