704bb6ce8cb715b1f0b7b629a3e639f4a33d5b22
Factory v2 <factory-v2@example.invalid>
2026-06-01 07:39:46 +0000

    factory-v2 task abb5349f-6c3b-476f-807b-1b1e914d0f96

--- test_app.py
+++ test_app.py
@@ -1,6 +1,11 @@
-from app import add
+from app import add, inc
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_inc():
+    assert inc(1) == 2
+    assert inc(-1) == 0
