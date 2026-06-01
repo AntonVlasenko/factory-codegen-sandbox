092bb1aaa0fa42cf583a872bb53378c92dd1946f
Factory v2 <factory-v2@example.invalid>
2026-06-01 08:35:42 +0000

    factory-v2 task 41a37e9b-21ce-4137-b4cb-a94e15513f27

--- test_app.py
+++ test_app.py
@@ -1,6 +1,11 @@
-from app import add
+from app import add, septuple
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_septuple():
+    assert septuple(3) == 21
+    assert septuple(-2) == -14
